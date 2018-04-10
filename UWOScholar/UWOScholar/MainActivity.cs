da8cde1c13c37e20b4436456ecbf0824b8c2a06f
Max Wahlgren <maxwahlgren@hotmail.com>
2018-04-10 17:28:27 -0500

    Worked on Login UI

--- UWOScholar/UWOScholar/MainActivity.cs
+++ UWOScholar/UWOScholar/MainActivity.cs
@@ -13,6 +13,18 @@ namespace UWOScholar
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var username = FindViewById<EditText>(Resource.Id.username);
+            var password = FindViewById<EditText>(Resource.Id.password);
+            Button btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            Button btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+            
+            btnLogin.Click += delegate {
+                if(username.ToString() == "Admin" && password.ToString() == "1234")
+                {
+                    SetContentView(Resource.Layout.Home);
+                }
+            };
         }
     }
 }
--- UWOScholar/UWOScholar/Resources/Resource.Designer.cs
+++ UWOScholar/UWOScholar/Resources/Resource.Designer.cs
@@ -41,11 +41,61 @@ namespace UWOScholar
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int UWOSeal = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int UWOSeal = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int btnLogin = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnRegister = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int password = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int relativeLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int username = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Home = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -60,8 +110,8 @@ namespace UWOScholar
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
