7599427020ccda2fa2d7093ef89c1412093bdd91
Max Wahlgren <wahlgm85@uwosh.edu>
2018-04-12 10:38:27 -0500

    Added Register button

--- UWOScholar/UWOScholar/MainActivity.cs
+++ UWOScholar/UWOScholar/MainActivity.cs
@@ -15,19 +15,24 @@ namespace UWOScholar
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-           
+            var username = (EditText)FindViewById(Resource.Id.username);
+            var password = (EditText)FindViewById(Resource.Id.password);
             Button btnLogin = (Button)FindViewById(Resource.Id.btnLogin);
             Button btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
             
             btnLogin.Click += delegate {
-                var username = (EditText)FindViewById(Resource.Id.username);
-                var password = (EditText)FindViewById(Resource.Id.password);
                 //if()
                 //{
                     Intent nextActivity = new Intent(this, typeof(HomeActivity));
 
                     StartActivity(nextActivity);
                 //}
+            };
+
+            btnRegister.Click += delegate
+            {
+                Intent registerActivity = new Intent(this, typeof(RegisterActivity));
+                StartActivity(registerActivity);
             };
         }
     }
--- UWOScholar/UWOScholar/Resources/Resource.Designer.cs
+++ UWOScholar/UWOScholar/Resources/Resource.Designer.cs
@@ -97,6 +97,9 @@ namespace UWOScholar
 			// aapt resource value: 0x7f030001
 			public const int Main = 2130903041;
 			
+			// aapt resource value: 0x7f030002
+			public const int Register = 2130903042;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
