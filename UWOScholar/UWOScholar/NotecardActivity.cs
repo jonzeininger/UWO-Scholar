1374286fca05b8570da7e267169128de0e0e5ec1
Matt Nelson <nelsom69@uwosh.edu>
2018-04-24 20:30:10 -0500

    Added notecard activity and began code for it

---
+++ UWOScholar/UWOScholar/NotecardActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UWOScholar
+{
+    [Activity(Label = "NotecardActivity")]
+    public class NotecardActivity : Activity
+    {
+        Button btnCreateCard;
+        Button btnEditCard;
+        EditText txtFront;
+        EditText txtBack;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+}
