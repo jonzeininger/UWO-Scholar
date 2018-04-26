ad4ab3fdac84539b2bec3d12a6c67cde508c83df
Max Wahlgren <wahlgm85@uwosh.edu>
2018-04-25 21:59:28 -0500

    test

--- UWOScholar/UWOScholar/FolderActivity.cs
+++ UWOScholar/UWOScholar/FolderActivity.cs
@@ -20,7 +20,8 @@ namespace UWOScholar
         Toolbar toolbarTop;
         Toolbar menuBottom;
         Button btnAddFolder;
-        private List<string> folders;
+        EditText txtFolderName;
+       // private List<string> folders;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,6 +30,13 @@ namespace UWOScholar
             SetContentView(Resource.Layout.Folder);
 
             menuBottom = FindViewById<Toolbar>(Resource.Id.menu);
+            toolbarTop = FindViewById<Toolbar>(Resource.Id.toolbar);
+            txtFolderName = FindViewById<EditText>(Resource.Id.txtName);
+            btnAddFolder = FindViewById<Button>(Resource.Id.btnAddFolder);
+
+
+            //Bottom Menu Bar setting and giving it functionality
+            //Probably should make a class that does this so we can call it on each page.
             menuBottom.Title = "Menu";
             menuBottom.InflateMenu(Resource.Menu.pageMenu);
             menuBottom.MenuItemClick += (sender, e) =>
@@ -46,11 +54,15 @@ namespace UWOScholar
                 }
             };
 
-            toolbarTop = FindViewById<Toolbar>(Resource.Id.toolbar);
+            //Setting the Top Toolbar and gving it the name My BackPack
             SetActionBar(toolbarTop);
             ActionBar.Title = "My Backpack";
 
+            //btnAddFolder.Click += BtnAddFolder_Click;
+
         }
-          
+
+        //private Button BtnAddFolder_Click(object sender, System.EventArgs e)
+      
     }
 }
